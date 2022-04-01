c7a88b97cfc81b209f8e742e87714dab2f7c90c0
jay <keangjay@gmail.com>
2022-04-01 16:14:03 +0800

    添加 nodePolyfills 解决项目报错

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import nodePolyfills from 'rollup-plugin-polyfill-node'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [
+    vue(),
+    {
+      ...nodePolyfills ({ include: ['node_modules/**/*.js', /node_modules\/.vite\/.*js/] }),
+      apply: 'serve'
+    }
+  ]
 })
