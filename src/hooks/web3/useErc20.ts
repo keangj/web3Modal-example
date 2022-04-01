454d98743730f80c8eefcc39b610f2112e3a5ce8
jay <keangjay@gmail.com>
2022-04-01 16:16:35 +0800

    封装代币方法 hooks

---
+++ src/hooks/web3/useErc20.ts
@@ -0,0 +1,48 @@
+import { useWallet } from "./useWallet"
+import { computed } from "vue";
+import { utils } from 'web3/dist/web3.min.js'
+
+const { web3 } = useWallet()
+const abi = [{ "inputs": [], "stateMutability": "nonpayable", "type": "constructor" }, { "anonymous": false, "inputs": [{ "indexed": true, "internalType": "address", "name": "owner", "type": "address" }, { "indexed": true, "internalType": "address", "name": "spender", "type": "address" }, { "indexed": false, "internalType": "uint256", "name": "value", "type": "uint256" }], "name": "Approval", "type": "event" }, { "anonymous": false, "inputs": [{ "indexed": true, "internalType": "address", "name": "previousOwner", "type": "address" }, { "indexed": true, "internalType": "address", "name": "newOwner", "type": "address" }], "name": "OwnershipTransferred", "type": "event" }, { "anonymous": false, "inputs": [{ "indexed": true, "internalType": "address", "name": "from", "type": "address" }, { "indexed": true, "internalType": "address", "name": "to", "type": "address" }, { "indexed": false, "internalType": "uint256", "name": "value", "type": "uint256" }], "name": "Transfer", "type": "event" }, { "inputs": [{ "internalType": "address", "name": "owner", "type": "address" }, { "internalType": "address", "name": "spender", "type": "address" }], "name": "allowance", "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }], "stateMutability": "view", "type": "function" }, { "inputs": [{ "internalType": "address", "name": "spender", "type": "address" }, { "internalType": "uint256", "name": "amount", "type": "uint256" }], "name": "approve", "outputs": [{ "internalType": "bool", "name": "", "type": "bool" }], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [{ "internalType": "address", "name": "account", "type": "address" }], "name": "balanceOf", "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "decimals", "outputs": [{ "internalType": "uint8", "name": "", "type": "uint8" }], "stateMutability": "view", "type": "function" }, { "inputs": [{ "internalType": "address", "name": "spender", "type": "address" }, { "internalType": "uint256", "name": "subtractedValue", "type": "uint256" }], "name": "decreaseAllowance", "outputs": [{ "internalType": "bool", "name": "", "type": "bool" }], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [{ "internalType": "address", "name": "spender", "type": "address" }, { "internalType": "uint256", "name": "addedValue", "type": "uint256" }], "name": "increaseAllowance", "outputs": [{ "internalType": "bool", "name": "", "type": "bool" }], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [{ "internalType": "uint256", "name": "amount", "type": "uint256" }], "name": "mint", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [], "name": "name", "outputs": [{ "internalType": "string", "name": "", "type": "string" }], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "owner", "outputs": [{ "internalType": "address", "name": "", "type": "address" }], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "renounceOwnership", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [], "name": "symbol", "outputs": [{ "internalType": "string", "name": "", "type": "string" }], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "totalSupply", "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }], "stateMutability": "view", "type": "function" }, { "inputs": [{ "internalType": "address", "name": "recipient", "type": "address" }, { "internalType": "uint256", "name": "amount", "type": "uint256" }], "name": "transfer", "outputs": [{ "internalType": "bool", "name": "", "type": "bool" }], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [{ "internalType": "address", "name": "sender", "type": "address" }, { "internalType": "address", "name": "recipient", "type": "address" }, { "internalType": "uint256", "name": "amount", "type": "uint256" }], "name": "transferFrom", "outputs": [{ "internalType": "bool", "name": "", "type": "bool" }], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [{ "internalType": "address", "name": "newOwner", "type": "address" }], "name": "transferOwnership", "outputs": [], "stateMutability": "nonpayable", "type": "function" }]
+type ERC20ContractType = 'busd'
+const contracts = {
+  busd: '0x7F38297507F95F7395D1B8fEC7140fAA3Dbe854c' // 测试网:0x7F38297507F95F7395D1B8fEC7140fAA3Dbe854c 主网:0xe9e7cea3dedca5984780bafc599bd69add087d56
+  // other contract
+}
+
+export function useERC20(contractType: ERC20ContractType) {
+  const contractAddress = contracts[contractType]
+  const erc20Instance = computed(() => {
+    const { Contract } = web3.value.eth
+    return new Contract(abi, contractAddress)
+  })
+
+  function getBalance(address: string): Promise<string> {
+    return new Promise((resolve, reject) => {
+      erc20Instance.value?.methods
+        .balanceOf(address).call()
+        .then((res: string) => resolve(utils.fromWei(res)))
+        .catch((error: Error) => reject(error))
+    })
+  }
+  async function approve(address: string, price: number) {
+    const [account] = await web3.value.eth.getAccounts()
+    const balance = await getBalance(account)
+    if (Number.parseFloat(balance) < price) {
+      return Promise.reject(new Error('Insufficient balance'))
+    }
+    return new Promise((resolve, reject) => {
+      erc20Instance.value.methods
+        .approve(address, (utils.toWei(price.toString())))
+        .send({ from: account })
+        .then((res: any) => {
+          resolve(res)
+        })
+        .catch((error: Error) => reject(error))
+    })
+  }
+  return {
+    getBalance,
+    approve
+  }
+}
